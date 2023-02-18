982b98460517a936fd9a01b438fb97df80be4a48
destors <halen54001@gmail.com>
2023-02-19 00:03:23 +0200

    update cta title

---
+++ death-stranding-screenshot/src/app/libs/landing/home/ui/main-cta/data/main-cta.txt.ts
@@ -0,0 +1,2 @@
+export const mainCtaDesc = `A fan made website with a collection, <br />
+of personal screenshots`;
--- death-stranding-screenshot/src/app/libs/landing/home/ui/main-cta/main-cta.component.ts
+++ death-stranding-screenshot/src/app/libs/landing/home/ui/main-cta/main-cta.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { mainCtaDesc } from './data/main-cta.txt';
 
 @Component({
   selector: 'app-main-cta',
   templateUrl: './main-cta.component.html',
-  styleUrls: ['./main-cta.component.scss']
+  styleUrls: ['./main-cta.component.scss'],
 })
 export class MainCtaComponent {
-
+  mainCtaDesc = mainCtaDesc;
 }
