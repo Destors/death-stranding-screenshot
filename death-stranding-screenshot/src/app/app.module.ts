121fc3b95abcc34d68eae98f07e9945536f4b799
destors <halen54001@gmail.com>
2023-03-07 23:44:19 +0200

    add firebase

--- death-stranding-screenshot/src/app/app.module.ts
+++ death-stranding-screenshot/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+  ],
+  providers: [ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ death-stranding-screenshot/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'death-stranding-screenshots',
+    appId: '1:962745828232:web:d8c650fb2a4cbb5fa3f03d',
+    storageBucket: 'death-stranding-screenshots.appspot.com',
+    apiKey: 'AIzaSyA5OVqndabO7YiFA7N6Y6lcEDOS8HNhcqM',
+    authDomain: 'death-stranding-screenshots.firebaseapp.com',
+    messagingSenderId: '962745828232',
+    measurementId: 'G-6NT4BWG33Q',
+  },};
